f470b79a73d3c411bdbfa9f24f813d0ed7aeb293
Pia Malone <piamalone@yahoo.com>
2023-10-24 03:41:51 +0000

    lists2

---
+++ lists2.py
@@ -0,0 +1,37 @@
+# Import the 'random' module for random number generation.
+import random
+
+# Create an empty dictionary named 'var'.
+var = {}
+
+# Print the type of 'var', which will show it's a dictionary.
+print(type(var))
+
+# Create a dictionary 'var2' with invalid syntax, it should use colons to separate keys and values.
+var2 = { "grocery list:" "apples", "oranges", "broccoli", "chicken", "water"}
+
+# Create a list named 'grocery_list' with grocery items.
+grocery_list = ["apples", "oranges", "broccoli", "chicken", "water"]
+
+# Append "vitamins" to the 'grocery_list'.
+grocery_list.append("vitamins")
+
+# Print the 'grocery_list' after adding "vitamins".
+print(grocery_list)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
