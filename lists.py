ca51adcc83e01f8c02a3b699e0080f284fce8d14
Pia Malone <piamalone@yahoo.com>
2023-10-24 03:20:37 +0000

    lists

---
+++ lists.py
@@ -0,0 +1,32 @@
+import random
+
+var = {}
+
+print (type(var))
+
+var2 = { "grocery list:" "apples", "oranges", "broccoli", "chicken", "water"}
+
+grocery_list = ["apples", "oranges", "broccoli", "chicken", "water"]
+
+grocery_list.append("vitamins")
+
+print(grocery_list)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
